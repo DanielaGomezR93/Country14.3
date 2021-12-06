5c3134d73e0cb357704398f86f4f8606cfceca8e
Raiver <raiver.figueroa@binaural.com.ve>
2021-12-06 16:27:19 -0400

    feat:anticipos en reporte audrey

--- custom/country_facturacion_campos/wizard/cuentas_por_cobrar_country.py
+++ custom/country_facturacion_campos/wizard/cuentas_por_cobrar_country.py
@@ -202,10 +202,10 @@ class CuentaPorCobrarCountryClub(models.TransientModel):
 		#print("monto a buscar para el partner",partner.name)
 		#print("para el periodo",m)
 		#print("arreglo de pagos",payments)
-		_logger.info("TOPDOS LOS PAGOS %s",payments)
+		#_logger.info("TOPDOS LOS PAGOS %s",payments)
 		payments_for_payment = payments.filtered(lambda p: p.partner_id.id == partner.id)
 		#print("payments_for_payment",payments_for_payment)
-		_logger.info("FILTRADO POR PARTNER %s",payments_for_payment)
+		#_logger.info("FILTRADO POR PARTNER %s",payments_for_payment)
 		amount = 0
 		amount_usd = 0
 		acum_advance_bs = 0
@@ -221,6 +221,23 @@ class CuentaPorCobrarCountryClub(models.TransientModel):
 				#nota: el anticipo no se cruza directo con factura entonces hay que buscar por otro lado
 				_logger.info("ES ANTICIPOOOOOOOOOOOOOOOOOOOOOOOOOOOOO %s",pp.name)
 				_logger.info("reconciled_invoice_ids ANTICIPOOOOOOOOOOOOOOOOOOOOOOOOOOOOO %s",pp.reconciled_invoice_ids)
+				lines_advance = self.env['account.move.line'].sudo().search([('payment_id_advance','=',pp.id),('move_id.state','=','posted')])
+				_logger.info("line_idsline_idsline_idsline_idsline_ids %s",pp.line_ids)
+				_logger.info("lines_advancelines_advancelines_advancelines_advancelines_advance %s",lines_advance)
+				invoices = self.env['account.move'].sudo().search([('state','=','posted'),('payment_state','in',['paid','in_payment']),('partner_id','=',pp.partner_id.id)])
+				for i in invoices:
+					if i.fee_period and i.fee_period.month == m.month and i.fee_period.year == m.year:
+						for ip in lines_advance:
+							amount_match = sum([p.amount for p in ip.matched_debit_ids if p.debit_move_id in i.line_ids])
+							if not pp.journal_id.currency_id or pp.journal_id.currency_id.id != alternate_currency_id.id: 
+								amount_usd +=amount_match
+								if pp.is_advance:
+									acum_advance_usd += amount_match
+							else:
+								amount +=amount_match
+								if pp.is_advance:
+									acum_advance_bs += amount_match
+
 			for i in pp.reconciled_invoice_ids:
 				if i.fee_period and i.fee_period.month == m.month and i.fee_period.year == m.year:
 					#amount += pp.amount
@@ -229,6 +246,7 @@ class CuentaPorCobrarCountryClub(models.TransientModel):
 					#reconciled_amls = reconciled_lines.mapped('matched_debit_ids.debit_move_id') + \
 					#   reconciled_lines.mapped('matched_credit_ids.credit_move_id')
 					_logger.info("reconciled_linesreconciled_linesreconciled_lines %s",reconciled_lines)
+					
 					for ip in reconciled_lines:
 						_logger.info("SUMARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
 						#print("esta linea asociada a factura tiene el pago",ip.payment_id)
